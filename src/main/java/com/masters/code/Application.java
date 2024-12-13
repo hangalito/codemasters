70756a01426837ef3b4ccdabc5df26446f4c1588
Bartolomeu Hangalo <bartolomeujose.manilson@gmail.com>
2024-12-13 03:12:00 +0100

    Initial commit

---
+++ src/main/java/com/masters/code/Application.java
@@ -0,0 +1,12 @@
+package com.masters.code;
+
+import org.springframework.boot.SpringApplication;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}

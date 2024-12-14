58dd55477f39abe18d4bc01576a102c097126e18
Bartolomeu Hangalo <bartolomeujose.manilson@gmail.com>
2024-12-14 04:34:57 +0100

    Adapt code to Spring Boot

---
+++ src/main/java/com/masters/code/config/WebConfig.java
@@ -0,0 +1,29 @@
+package com.masters.code.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/about").setViewName("sobre");
+        registry.addViewController("/nosso-metodo").setViewName("metodo");
+        registry.addViewController("/comunidade").setViewName("comunidade");
+        registry.addViewController("/cursos").setViewName("cursos");
+        registry.addViewController("/suporte").setViewName("suporte");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/");
+    }
+}

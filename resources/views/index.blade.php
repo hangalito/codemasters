17c58346731ed2d2c8da8a66fb056833b6c10e24
Bartolomeu Hangalo <bartolomeujose.manilson@gmail.com>
2025-04-16 19:17:37 +0100

    landing page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,19 +1,33 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <title>Code Masters</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="{{asset('favicon.png')}}" type="image/x-icon">
+@extends('app.layout')
+@section('title','Início')
+@section('stylesheets')
+    <link rel="stylesheet" href="{{asset('css/styles.css')}}">
     @vite('resources/css/app.css')
-    @vite('resources/js/app.js')
-</head>
-<body>
-<x-navbar active-page="home"/>
-<div class="text-lg font-bold border-0 border-b-blue-300 bg-clip-padding">
-    Desejas aprender a programa?
-    Temos um convite para ti!
-</div>
-</body>
-</html>
+@endsection
+@section('content')
+    <div id="top" class="hero text-white flex gap-6 p-12 items-center">
+        <div class="hero-content flex flex-col max-w-2xl flex-wrap gap-1">
+            <h1 class="text-4xl">Desejas tornar-te um desenvolvedor de software?</h1>
+            <h2 class="text-3xl text-gray-300">Temos um convite para ti!</h2>
+            <p class="text-justify">
+                Transforme sua paixão por tecnologia em uma carreira de sucesso! No nosso site, oferecemos uma
+                jornada completa, desde os fundamentos até as técnicas mais avançadas de programação com materiais
+                de alta qualidade, incluindo PDFs detalhados e vídeos tutoriais, você terá todas as ferramentas
+                necessárias para se destacar no mundo da programação.
+            </p>
+            <a class="bg-green-600 text-2xl text-center max-w-2/6 pt-1.5 pb-1.5 rounded-md mt-3 mb-3 hover:bg-green-800 transition-colors"
+               href="{{route('register')}}">
+                Inscrever-se
+            </a>
+        </div>
+        <div class="hero-img-container not-sm:hidden">
+            <img class="transform-cpu" src="{{asset('img/tech-man.png')}}" alt="Futurista">
+        </div>
+    </div>
+    <div id="cursos">
+        <h1 class="text-white text-4xl text-center">Nossos cursos</h1>
+        @foreach($cursos as $curso)
+            {{ $curso->nome}}
+        @endforeach
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'index')->name('home');
+Route::get('/', HomeController::class)->name('home');
+
 Route::view('about', 'about')->name('about');
 Route::view('courses', 'courses')->name('courses');
 Route::view('support', 'support')->name('support');

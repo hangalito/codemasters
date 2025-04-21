834863fdab2994396b129cb5f12c9628d24b5fe3
Bartolomeu Hangalo <bartolomeujose.manilson@gmail.com>
2025-04-22 00:07:39 +0100

    add favicon

--- resources/views/app/layout.blade.php
+++ resources/views/app/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title') | {{ config('app.name') }}</title>
+    <link rel="shortcut icon" href="{{asset('favicon.png')}}" type="image/png">
     @yield('stylesheets')
 </head>
 <body>

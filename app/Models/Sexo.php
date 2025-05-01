14f79de29ef369fe3ad41cb75bde0a254ad25197
Bartolomeu Hangalo <bartolomeujose.manilson@gmail.com>
2025-05-01 01:57:11 +0100

    add database seeder for table `sexos`

--- app/Models/Sexo.php
+++ app/Models/Sexo.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sexo extends Model
 {
+
+    protected $fillable = [
+        "id",
+        "desc"
+    ];
+
     public function alunos()
     {
         return $this->hasMany(Aluno::class);
